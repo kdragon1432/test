f97f23b9a01fc0abd5b709699d369004455f4b37
kdragon1432 <kellydong1432@gmail.com>
2017-08-16 15:08:41 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
 #! user/bin/env python
 
 import cgi
-print """Content-type: text/html
-<!DOCTYPE html>
-<html>
-hello world
-</html>"""
+print("Content-type: text/html <!DOCTYPE html> <html> hello world </html>")
